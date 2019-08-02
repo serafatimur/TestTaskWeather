a155c3fb6606a5e016e6592c065d33a7f4e9abb3
serafatimur <serafatimur@gmail.com>
2019-08-02 08:18:52 +0300

    bug fixed and somechange

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,34 +1,25 @@
-let todayButton;
-let tomorrowButton;
-let dayAfterTomorrowButton;
+let daysElements;
 let selectedDayElement;
+let circleWrapperArray;
+let celsiusWrapperArray;
+let iconsArray;
 
 window.onload = function () {
-    resize();
-    todayButton = document.getElementById("today");
-    tomorrowButton = document.getElementById("tomorrow");
-    dayAfterTomorrowButton = document.getElementById("dayAfterTomorrow");
+    daysChildrenElementsArray = document.getElementById("days").children;
     selectedDayElement = document.getElementById("selectedDay");
-
-    tomorrowButton.onclick = function() {
-        translateX("-33.3333%");
-        tomorrowButton.classList.add("selected");
-        todayButton.classList.remove("selected")
-        dayAfterTomorrowButton.classList.remove("selected");
-    }
-
-    todayButton.onclick = function() {
-        translateX("0");
-        tomorrowButton.classList.remove("selected");
-        todayButton.classList.add("selected")
-        dayAfterTomorrowButton.classList.remove("selected");
-    }
-
-    dayAfterTomorrowButton.onclick = function() {
-        translateX("-66.6666%")
-        tomorrowButton.classList.remove("selected");
-        todayButton.classList.remove("selected")
-        dayAfterTomorrowButton.classList.add("selected");
+    circleWrapperArray = document.getElementsByClassName("precipitationImage");
+    celsiusWrapperArray = document.getElementsByClassName("temperature");
+    iconsArray = document.getElementsByClassName("icon");
+    resize();
+    
+    for (let i = 0; i < daysChildrenElementsArray.length; i++) {
+        daysChildrenElementsArray[i].onclick = function()  {
+            translateX(-i*100+"vw");
+            for (let j = 0; j < daysChildrenElementsArray.length; j ++) {
+                daysChildrenElementsArray[j].classList.remove("selected");
+            }
+            daysChildrenElementsArray[i].classList.add("selected");
+        }
     }
 }
 
@@ -41,9 +32,6 @@ function resize() {
     let circleWidth = 14 / ratio;
     let celsiusWidth = 3 / ratio;
     let iconWidth = 50 / ratio;
-    let circleWrapperArray = document.getElementsByClassName("precipitationImage");
-    let celsiusWrapperArray = document.getElementsByClassName("temperature");
-    let iconsArray = document.getElementsByClassName("icon");
 
     if (circleWidth > 36) {
         circleWidth = 36;
